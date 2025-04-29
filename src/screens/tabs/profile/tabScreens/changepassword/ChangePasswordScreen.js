7c3835614144a898840ce4de30070dbe40a7616c
advantalaaryant <aaryant.malviya@advantal.net>
2025-04-29 11:21:11 +0530

    ic launcher added

--- src/screens/tabs/profile/tabScreens/changepassword/ChangePasswordScreen.js
+++ src/screens/tabs/profile/tabScreens/changepassword/ChangePasswordScreen.js
@@ -58,9 +58,15 @@ const ChangePasswordScreen = (props) => {
             email: props.userData?.email,
             password: currentPassword,
             newPassword: newPassword,
-            token: props?.userData?.token,
-            userType: "DOCTOR"
+            token: props?.authToken,
         };
+        // const reqParam = {
+        //     "email": data.email,
+        //     "password": data.password,
+        //     "newPassword": data.newPassword,
+        //     "token": data.token,
+        //     "userType": "DOCTOR"
+        //   }
         console.log(reqParams, 'reqparam')
         await props.ChangePassowordAction(reqParams);
     };
@@ -181,6 +187,7 @@ const mapStateToProps = (state) => ({
     errMsg: state.authReducer.errMsg,
     changePasswordResponseCode: state.authReducer.changePasswordResponseCode,
     userData: state.authReducer.userData,
+    authToken: state.authReducer.authToken,
 });
 
 const mapDispatchToProps = {
@@ -189,248 +196,4 @@ const mapDispatchToProps = {
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(ChangePasswordScreen);
-
-
-// import React, { useState } from 'react';
-// import {
-//     View,
-//     Text,
-//     Image,
-//     ImageBackground,
-//     TouchableOpacity,
-// } from "react-native";
-// import {
-//     Images,
-//     Colors,
-//     WindowHeight as hp,
-//     Fonts,
-//     ResponsiveFont
-// } from '../../../../../assets';
-// import { FloatingBackgroundCard } from '../../../../../components/card';
-// import { useTranslation } from '../../../../../components/customhooks';
-// import { CustomTextInput } from '../../../../../components/input';
-// import { CustomButton } from '../../../../../components/button';
-// import { ChangePasswordStyles } from './ChangePasswordStyles';
-// import { validatePassword } from '../../../../../utility/Validator';
-// import { ChangePassowordAction } from '../../../../../Redux/actions';
-// import { connect } from 'react-redux';
-// const ChangePasswordScreen = (props) => {
-//     const t = useTranslation();
-
-//     const [currentPassword, setCurrentPassword] = useState('');
-//     const [newPassword, setNewPassword] = useState('');
-//     const [confirmPassword, setConfirmPassword] = useState('');
-
-//     const [isCurrentValid, setIsCurrentValid] = useState(null);
-//     const [isNewValid, setIsNewValid] = useState(false);
-//     const [isConfirmMatch, setIsConfirmMatch] = useState(false);
-
-//   // Validation Handlers
-// const handleCurrentPassword = (text) => {
-//     setCurrentPassword(text);
-//     setIsCurrentValid(text === '123456'); // Dummy check
-// };
-
-// const handleNewPassword = (text) => {
-//     setNewPassword(text);
-//     const isValid = validatePassword(text);
-//     setIsNewValid(isValid);
-//     // Also update confirm match if confirm already has value
-//     if (confirmPassword.length > 0) {
-//         setIsConfirmMatch(text === confirmPassword);
-//     }
-// };
-
-// const handleConfirmPassword = (text) => {
-//     setConfirmPassword(text);
-//     setIsConfirmMatch(text === newPassword);
-// };
-
-
-//     const handleChangePassword= async () => {
-//         if (!currentPassword) {
-//             ToastMsg('Please enter current password', 'bottom');
-//             return false;
-//         }
-//         if (!isCurrentValid) {
-//             ToastMsg('Please enter valid password', 'bottom');
-//             return false;
-//         }
-//         if (!newPassword) {
-//             ToastMsg('Please enter new paasword', 'bottom');
-//             return false;
-//         }
-//         if (!confirmPassword) {
-//             ToastMsg('Please enter confirm paasword', 'bottom');
-//             return false;
-//         }
-//         if (!isNewValid) {
-//             ToastMsg('New password must be at least 8 characters', 'bottom');
-//             return;
-//         }
-//         if (newPassword!==confirmPassword) {
-//             ToastMsg('new password and confirm password should be same', 'bottom');
-//             return false;
-//         }
-//         const reqParams = {
-//             "email": props.userData.email,
-//             "password": currentPassword,
-//             "newPassword": newPassword,
-//             "token": props?.userData?.token,
-//             "userType": "DOCTOR"
-//         }
-//         // await props.ChangePassowordAction(reqParams);
-//         console.log(reqParams,'reqparam')
-//     }
-
-//     return (
-//         <ImageBackground
-//             source={Images.backgroundImage}
-//             style={ChangePasswordStyles.background}
-//             resizeMode="cover"
-//         >
-//             <View style={ChangePasswordStyles.topView}>
-//                 <TouchableOpacity
-//                     style={ChangePasswordStyles.tabNameContainer1}
-//                     onPress={() => props.navigation.goBack()}
-//                 >
-//                     <Image source={Images.back_Icon} style={ChangePasswordStyles.backIcon}/>
-//                 </TouchableOpacity>
-//                 <View style={ChangePasswordStyles.tabNameContainer}>
-//                     <Text style={ChangePasswordStyles.tabName}>Change Password</Text>
-//                 </View>
-//             </View>
-
-//             <View style={ChangePasswordStyles.bottomView}>
-//                 <FloatingBackgroundCard>
-//                     <View style={{ width: '100%' }}>
-//                         <View style={ChangePasswordStyles.innerContent}>
-//                             <Image
-//                                 source={Images.changePasswordIcon}
-//                                 style={ChangePasswordStyles.lockImage}
-//                             />
-//                             <Text style={ChangePasswordStyles.changePasswordText}>Change Password</Text>
-
-//                             {/* Current Password */}
-//                             <View style={ChangePasswordStyles.inputWrapper}>
-//                                 <CustomTextInput
-//                                     heading="Current Password"
-//                                     placeholder="Enter Current Password"
-//                                     value={currentPassword}
-//                                     onChangeText={handleCurrentPassword}
-//                                     type="password"
-//                                     width="100%"
-//                                 />
-//                             </View>
-
-//                             {/* New Password */}
-//                             <View style={ChangePasswordStyles.inputWrapper}>
-//                                 <CustomTextInput
-//                                     heading="New Password"
-//                                     placeholder="Enter New Password"
-//                                     value={newPassword}
-//                                     onChangeText={handleNewPassword}
-//                                     type="password"
-//                                     width="100%"
-//                                 />
-//                             </View>
-
-//                             {/* Confirm New Password */}
-//                             <View style={ChangePasswordStyles.inputWrapper}>
-//                                 <CustomTextInput
-//                                     heading="Confirm New Password"
-//                                     placeholder="Confirm New Password"
-//                                     value={confirmPassword}
-//                                     onChangeText={handleConfirmPassword}
-//                                     type="password"
-//                                     width="100%"
-//                                 />
-//                             </View>
-
-//                             <CustomButton
-//                                 title="Change Password"
-//                                 onPress={handleChangePassword}
-//                                 backgroundColor={Colors.blue}
-//                                 textColor={Colors.white}
-//                                 width="100%"
-//                                 marginVertical="5%"
-//                             // disabled={!isFormValid}
-//                             />
-//                         </View>
-//                     </View>
-
-//                 </FloatingBackgroundCard>
-//             </View>
-//         </ImageBackground>
-//     );
-// };
-
-// const mapStateToProps = state => {
-//     return {
-//         loading: state.authReducer.loading,
-//         errMsg: state.authReducer.errMsg,
-//         changePasswordResponseCode: state.authReducer.changePasswordResponseCode,
-//         userData: state.authReducer.userData,
-//     };
-// };
-
-// const mapDispatchToProps = {
-//     ChangePassowordAction
-// };
-// export default connect(mapStateToProps, mapDispatchToProps)(ChangePasswordScreen);
-
-
-// import React, { useState } from 'react';
-// import {
-//     View,
-//     Text,
-//     ImageBackground,
-//     FlatList,
-//     TouchableOpacity,
-//     Image,
-// } from "react-native";
-// import {
-//     Images,
-//     Colors,
-//     WindowHeight as hp,
-//     Fonts,
-//     ResponsiveFont
-// } from '../../../../../assets';
-// import { FloatingBackgroundCard } from '../../../../../components/card';
-// import { useTranslation } from '../../../../../components/customhooks';
-// import { ChangePasswordStyles } from './ChangePasswordStyles';
-
-// const ChangePasswordScreen = (props) => {
-//     const t = useTranslation();
-
-//     return (
-//         <ImageBackground
-//             source={Images.backgroundImage}
-//             style={ChangePasswordStyles.background}
-//             resizeMode="cover"
-//         >
-//             <View style={ChangePasswordStyles.topView}>
-//                 <TouchableOpacity
-//                     style={ChangePasswordStyles.tabNameContainer1}
-//                     onPress={() => props.navigation.goBack()}>
-//                     <Image
-//                         source={Images.back_Icon}
-//                         style={ChangePasswordStyles.backIcon}
-//                     />
-//                 </TouchableOpacity>
-//                 <View style={ChangePasswordStyles.tabNameContainer}>
-//                     <Text style={ChangePasswordStyles.tabName}>Change Password</Text>
-//                 </View>
-//             </View>
-
-//             <View style={ChangePasswordStyles.bottomView}>
-//                 <FloatingBackgroundCard>
-//                 </FloatingBackgroundCard>
-//             </View>
-//         </ImageBackground>
-//     );
-// };
-
-// export default ChangePasswordScreen;
-
 
